57a6f284e4ebec17b3796849c27c86aff7c30688
ulfahmufi <ulfahmufidatulkhoiriyah@mail.ugm.ac.id>
2025-10-28 23:20:21 +0700

    Update week 9 nomor 3a.py

--- week 9 nomor 3a.py
+++ week 9 nomor 3a.py
@@ -1,12 +1,14 @@
-import NumPy as np
+import numpy as np
 
 A = np.array([[2, 4, 6],
               [1, 3, 5]])
 
 B = np.array([[1, 1, 1],
               [2, 2, 2]])
-tambah = np.add(A, B)
-kurang = np.substract(A, B)
+
+# Penjumlahan dan pengurangan
+tambah = A + B
+kurang = A - B
 
 print("Penjumlahan Matriks A + B:\n", tambah)
-print("Pengurangan Matriks A - B:\n", kurang)
+print("\nPengurangan Matriks A - B:\n", kurang)
